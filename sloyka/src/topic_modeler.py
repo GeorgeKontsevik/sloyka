90af484af67fccdc74e1551812e78f87837b58f0
Sandrro <antonov-as@yandex.ru>
2024-04-24 15:34:05 +0300

    fixed bugs

--- sloyka/src/topic_modeler.py
+++ sloyka/src/topic_modeler.py
@@ -21,45 +21,62 @@ from transformers.pipelines import pipeline
 from loguru import logger
 
 class TopicModeler:
-    def __init__(self, gdf, start_date, end_date, embedding_model_name="cointegrated/rubert-tiny2"):
+    def __init__(self, gdf, start_date, end_date, min_texts=15, embedding_model_name="cointegrated/rubert-tiny2"):
         self.gdf = gdf
+        self.MIN_TEXTS = min_texts
         self.gdf.text = self.gdf.text.astype(str)
+        self.gdf.date = pd.to_datetime(self.gdf.date)
         self.gdf.date = self.gdf.date.dt.date
         
         self.embedding_model = pipeline("feature-extraction", model=embedding_model_name)
-        self.umap_model = UMAP(n_neighbors=15, n_components=5, min_dist=0.0, metric='cosine', random_state=42)
-        self.date_range = pd.date_range(start_date, end_date, freq='D')
-        self.global_model = None
-        self.df_outliers = None
+        self.umap_model = UMAP(n_neighbors=self.MIN_TEXTS, n_components=5, min_dist=0.0, metric='cosine', random_state=42)
+        self.date_range = pd.date_range(pd.to_datetime(start_date), pd.to_datetime(end_date), freq='D')
 
     def process_topics(self):
         df_list = []
         outlier_list = []
+        global_model = None
 
         for single_date in self.date_range:
+            
             # Loop through each day in the date range
             logger.info(f"Processing date: {single_date}")
             # Filter DataFrame for the current date
-            current_df = self.filter_data_by_date(single_date.date())
-            if self.df_outliers is not None:
-                current_df = pd.concat([current_df, self.df_outliers])
+            current_df = self.gdf[self.gdf['date'] == single_date.date()]
+            if len(outlier_list) != 0:
+                outlier_list.append(current_df)
+                current_df = pd.concat(outlier_list)
+                outlier_list = []
             docs = current_df.text.to_list()
             # Create clusters for the current date
-            current_topic_model = self.create_topic_model(docs)
-
-            if single_date.date() == self.date_range[0].date():
-                self.global_model = current_topic_model
+            if len(current_df) >= self.MIN_TEXTS:
+                current_model = BERTopic(embedding_model=self.embedding_model, umap_model=self.umap_model).fit(docs)
             else:
-                self.global_model = BERTopic.merge_models([self.global_model, current_topic_model], min_similarity=0.9)
+                outlier_list.append(current_df)
+                logger.info(f"Not enough texts: {len(current_df)} for time period")
+                continue
+            if global_model is None:
+                # Global model is current model in the beginning of the period 
+                global_model = current_model
+            else:
+                # Global model is merged from previous and current models
+                global_model = BERTopic.merge_models([global_model, current_model], min_similarity=0.9)
             
-            current_df, self.df_outliers = self.handle_clusters(current_df, current_topic_model, docs)
+            # Create df with clusters for current model
+            current_df = current_model.get_document_info(docs, current_df)
+            # Filter outlier texts into separate df
+            df_outliers = current_df[current_df['Topic'] == -1].drop(columns=[
+                'Document', 'Topic', 'Name', 'Representation', 'Representative_Docs', 
+                'Top_n_words', 'Probability', 'Representative_document'])
+            outlier_list.append(df_outliers)
+            current_df = current_df[current_df["Topic"] != -1]
+            # Add current df without outliers to the list
             df_list.append(current_df)
 
         # Create final df with outliers left
-        df_list.append(self.df_outliers)
-        df_clusters = pd.concat(df_list)
+        df_clusters = pd.concat((df_list + outlier_list))
         # Create final df with outliers left
-        final_topics = self.global_model.get_topic_info()
+        final_topics = global_model.get_topic_info()
         final_topics.index = final_topics.Name
         df_clusters.drop(columns=['Topic'], inplace=True)
         df_clusters = df_clusters.join(final_topics['Topic'], on='Name')
@@ -70,26 +87,3 @@ class TopicModeler:
         df_clusters.rename(columns={'Name':'cluster_name', 
             'Topic':'cluster_id', 'Probability':'cluster_probability'}, inplace=True)
         return df_clusters
-
-    def filter_data_by_date(self, single_date):
-        return self.gdf[self.gdf['date'] == single_date]
-
-    def create_topic_model(self, docs):
-        return BERTopic(embedding_model=self.embedding_model, umap_model=self.umap_model).fit(docs)
-
-    def handle_clusters(self, df, topic_model, docs):
-        # Create df with clusters for current model
-        df = topic_model.get_document_info(docs, df)
-        # Filter outlier texts into separate df
-        df_outliers = df[df['Topic'] == -1].drop(columns=[
-            'Document', 'Topic', 'Name', 'Representation', 'Representative_Docs', 
-            'Top_n_words', 'Probability', 'Representative_document'])
-        df = df[df["Topic"] != -1]
-        return df, df_outliers
-
-# Example of using the class
-gdf = gpd.read_file("F:/Coding/all_parsed_11_04_2024_parser_1.1_fixed.geojson")
-start_date = '2024-03-01'
-end_date = '2024-03-05'
-topic_modeler = TopicModeler(gdf, start_date, end_date)
-df_clusters = topic_modeler.process_topics()
